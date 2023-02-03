58bf41ad2d8db238e98ee1cff2b3be9555023c0f
Tristian Chevier <80795010+tchevier@users.noreply.github.com>
2023-02-03 02:01:45 -0700

    update client axios url

--- client/src/services/internalApiService.js
+++ client/src/services/internalApiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: 'https://codecrackdown.com/'
 })
 
 export const getAllUsers = async() => {
